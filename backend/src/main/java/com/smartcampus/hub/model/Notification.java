0369d78827686d4da674662af129e8a734de2389
Vihanga09 <vihanga.shehan99@gmail.com>
2026-04-15 18:35:14 +0530

    add Notification model for user notifications in the system

---
+++ backend/src/main/java/com/smartcampus/hub/model/Notification.java
@@ -0,0 +1,30 @@
+package com.smartcampus.hub.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String message;
+
+    private String type;
+
+    private boolean isRead = false;
+
+    private LocalDateTime createdAt;
+}
